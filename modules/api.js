6ee8932df4c37eb576bdcda276c7a7831fa90aed
井上奈々 <nana@diffeasy.com>
2019-01-27 15:09:39 +0900

    update

--- modules/api.js
+++ modules/api.js
@@ -1,26 +1,21 @@
 const axios = require('axios')
-const express = require('express')
 
-module.exports = async function apiModule(moduleOptions) {
-  this.nuxt.hook('build:before', async ({ isStatic, isDev }) => {
+module.exports = async function asyncModule() {
 
-    console.log(process.env.FLAMELINK_WEBHOOK)
-    
-    // データ取得
-    const json = await axios.get(this.options.env.FLAMELINK_WEBHOOK)
+  // データ取得
+  const json = await axios.get(this.options.env.FLAMELINK_WEBHOOK)
 
-    console.log('=== json ===')
-    console.log(json.data.myblogList)
+  console.log('=== json ===')
+  console.log(json.data.myblogList)
 
-    // jsonに書き出す
-    this.options.build.plugins.push({
-      apply (compiler) {
-        compiler.plugin('emit', (compilation, cb) => {
-          // source はバッファとなる
-          compilation.assets['blog.json'] = { source: () => JSON.stringify(json.data.myblogList), size: () => 1537 }
-          cb()
-        })
-      }
-    })
+  // JSONファイル書出し
+  this.options.build.plugins.push({
+    apply (compiler) {
+      compiler.plugin('emit', (compilation, cb) => {
+        // source はバッファとなる
+        compilation.assets['blog.json'] = { source: () => JSON.stringify(json.data.myblogList), size: () => 1537 }
+        cb()
+      })
+    }
   })
 }
